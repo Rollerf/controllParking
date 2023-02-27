fa46a89a2fd120651513a18d25d36b73783b8c99
Chema <35430972+Rollerf@users.noreply.github.com>
2023-02-27 12:06:48 +0100

    Delete MQTTconfig.h

--- MQTTconfig.h
+++
@@ -1,8 +0,0 @@
-// MQTT Broker
-const char *mqtt_broker = "192.168.1.3";
-const char *topicState = "parking/state";
-const char *topicCommand = "parking/command";
-const char *mqtt_username = "mqttMosquitto";
-const char *mqtt_password = "mosquittoDevices.";
-const int mqtt_port = 1883;
-const char *client_name = "parking-";
